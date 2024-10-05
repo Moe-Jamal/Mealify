8c87451eb1099f9d709b08e06784f48cfb52d681
moe-jamal <moe.jamal.360@gmail.com>
2024-10-05 13:30:09 +0300

    footer added

---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+// dark mode 
+let btnSwitch = document.querySelector('.switch');
+let chefSvg = document.querySelectorAll('.overlay-chef');
+btnSwitch.onclick = function() {
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        btnSwitch.classList.remove("fa-moon");
+        btnSwitch.classList.add("fa-sun") ;
+        chefSvg.forEach( item => {
+            item.src = "./images/team-shape-dark.svg";
+        });
+    } else {
+        btnSwitch.classList.remove("fa-sun");
+        btnSwitch.classList.add("fa-moon");
+        chefSvg.forEach(item => {
+            item.src = "./images/team-shape.svg";
+        });
+    }
+}
+// Show / hide mobile-navbar 
+let navLinks = document.querySelector('.links')
+function showLinks() {
+    navLinks.style.cssText = "right: 0;";
+}
+function hideLinks() {
+    navLinks.style.cssText = "right: -100%;"
+}
+// Add / Remove active class to navbar links 
+let navLinkss = document.querySelectorAll('.section-link');
+navLinkss.forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.active').classList.remove('active');
+        link.style.cssText = "transition: none;"
+        link.classList.add('active')
+    })
+})
+
+// function resizeCheck() {
+//     if (window.innerWidth > 992) {
+//         navLinks.style.cssText = "display: flex;";
+//     } else if (window.innerWidth < 992) {
+//         navLinks.style.cssText = "display: none;";
+//     }
+// }
+// window.addEventListener('resize', resizeCheck)
